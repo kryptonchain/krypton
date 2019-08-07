3d9460256ab766e3aab340303ab3ca182ddcd6a0
GM <meszaros275@gmail.com>
2019-08-07 18:12:53 +0100

    updates

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -67,7 +67,10 @@ TurtleCoind --print-genesis-tx --genesis-block-reward-address TRTLv2Fyavy8CXG8BP
 * You should see your premine appear in the previously generated wallet.
 
 */
-const char     GENESIS_COINBASE_TX_HEX[]                     = "e001d093a1a196ba150000210105f41ba867e91b8baae56511a395175b10868440bda669c1df333e3c76fc543e";
+const char GENESIS_COINBASE_TX_HEX[] =
+            "010a01ff000188f3b501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210142694232c5b04151"
+            "d9e4c27d31ec7a68ea568b19488cfcb422659a07a0e44dd5";
+
 static_assert(sizeof(GENESIS_COINBASE_TX_HEX)/sizeof(*GENESIS_COINBASE_TX_HEX) != 1, "GENESIS_COINBASE_TX_HEX must not be empty.");
 
 /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
@@ -274,6 +277,6 @@ const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 };
 
 const char* const SEED_NODES[] = {
-  "108.61.173.111:12887", // GM
+  "104.238.186.210:12887", // GM
 };
 } // Krypton
