c1c07cff9391448b2d9e9fe3801b346621de9de8
GM <meszaros275@gmail.com>
2019-08-07 19:43:06 +0100

    change wallet prefix

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -11,7 +11,7 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string_view addressPrefix = "AKRY";
+    const std::string_view addressPrefix = "atom";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
     const std::string ticker = "KRYP";
